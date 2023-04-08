d2a5c86f471cb7240d0c72608aaca7cc6a0d7275
Daniel Gal <denxyzw@gmail.com>
2023-04-08 22:55:33 +0200

    query top msg ids

--- packages/picker/src/main.ts
+++ packages/picker/src/main.ts
@@ -1,8 +1,31 @@
-import Harvester from "@zipchat/harvester";
+import { getStash } from "@zipchat/database";
+import { z } from "zod";
+
+const stash = getStash();
 
 async function main() {
-  const harvester = new Harvester();
-  console.log(await harvester.harvest());
+  const topSchema = z.array(z.object({ id: z.string().cuid() }).strict());
+  const top = topSchema.parse(
+    await stash.$queryRaw`
+      SELECT id
+      FROM Message
+      INNER JOIN (
+        SELECT messageId, COUNT(*) as reaction_count
+        FROM Reaction
+        GROUP BY messageId
+        HAVING reaction_count >= 10
+      ) r ON Message.id = r.messageId;
+    `
+  );
+  console.log(top)
 }
 
-main();
+main()
+  .then(async () => {
+    await stash.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await stash.$disconnect();
+    process.exit(1);
+  });
