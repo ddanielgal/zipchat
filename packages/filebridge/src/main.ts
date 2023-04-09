516f687d38f3cd0f3647d7c87c87058c3346c7e5
Daniel Gal <denxyzw@gmail.com>
2023-04-10 00:43:53 +0200

    upload all files to supabase

--- packages/filebridge/src/main.ts
+++ packages/filebridge/src/main.ts
@@ -1,5 +1,8 @@
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import { getLive } from "@zipchat/database";
 import Harvester from "@zipchat/harvester";
+
+const db = getLive();
 
 class FileBridge {
   private harvester: Harvester;
@@ -14,17 +17,48 @@ class FileBridge {
   }
 
   async brige() {
-    const buffer = await this.harvester.getFile(
-      "messages/inbox/chat/image.png"
-    );
-    return await this.supabase.storage
-      .from("toplista")
-      .upload("messages/inbox/chat/image.png", buffer);
+    const photos = await db.photo.findMany();
+    const videos = await db.video.findMany();
+    const gifs = await db.gif.findMany();
+    const files = await db.file.findMany();
+    const audioFiles = await db.audioFile.findMany();
+    const stickers = await db.sticker.findMany();
+    const media = [
+      ...photos,
+      ...videos,
+      ...gifs,
+      ...files,
+      ...audioFiles,
+      ...stickers,
+    ];
+    for (const m of media) {
+      const buffer = await this.harvester.getFile(m.uri);
+      if (!buffer) {
+        console.error(`Could not find ${m.uri}`);
+        continue;
+      }
+      const result = await this.supabase.storage
+        .from("toplista")
+        .upload(m.uri, buffer);
+      if (result.error) {
+        console.error(`Could not upload ${m.uri}: ${result.error.message}`);
+        continue;
+      }
+      console.log(`Uploaded ${result.data.path}`);
+    }
   }
 }
 async function main() {
   const fileBridge = new FileBridge();
-  console.log(await fileBridge.brige());
+  await fileBridge.brige();
 }
 
-main();
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
