e72a03718bf77881da4e8458e5c385be75912178
Daniel Gal <denxyzw@gmail.com>
2023-04-09 22:51:10 +0200

    upload image to supabase storage

--- packages/filebridge/src/main.ts
+++ packages/filebridge/src/main.ts
@@ -1,19 +1,27 @@
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import Harvester from "@zipchat/harvester";
 
 class FileBridge {
   private harvester: Harvester;
+  private supabase: SupabaseClient;
 
   constructor() {
     this.harvester = new Harvester();
+    this.supabase = createClient(
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
+    );
   }
 
   async brige() {
-    return this.harvester.getFile(
+    const buffer = await this.harvester.getFile(
       "messages/inbox/chat/image.png"
     );
+    return await this.supabase.storage
+      .from("toplista")
+      .upload("messages/inbox/chat/image.png", buffer);
   }
 }
-
 async function main() {
   const fileBridge = new FileBridge();
   console.log(await fileBridge.brige());
